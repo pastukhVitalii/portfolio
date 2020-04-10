bb7fda61f4aa3dfe157d022e564673900a7cef35
pastukhVitalii <pastukh.v.7@gmail.com>
2020-04-10 19:23:23 +0300

    Updates

---
+++ precache-manifest.50c18ad773ec1a5f3cdba0fe9ab9eafa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70403178b45d04a71e5462f26989a043",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "21f4164b0b1320defbe6",
+    "url": "/portfolio/static/css/main.675bbc18.chunk.css"
+  },
+  {
+    "revision": "b1513208ae0a0e034200",
+    "url": "/portfolio/static/js/2.8431c622.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8431c622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f4164b0b1320defbe6",
+    "url": "/portfolio/static/js/main.8884317a.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "c3df1c0809511a0968523653fc946270",
+    "url": "/portfolio/static/media/blog.c3df1c08.PNG"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/portfolio/static/media/facebook-brands.62d8b6ef.svg"
+  },
+  {
+    "revision": "c45ce9e4087f93cd85abdb8e1c4c8283",
+    "url": "/portfolio/static/media/google-plus-brands.c45ce9e4.svg"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/portfolio/static/media/react-brands.d8cf7740.svg"
+  },
+  {
+    "revision": "79e74aaaa77877512f192ca315448d8b",
+    "url": "/portfolio/static/media/telegram-brands.79e74aaa.svg"
+  }
+]);
